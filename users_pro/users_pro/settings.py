666c3b4042f4bf33cd023fa726879c0a0eb027dc
Mittapelly-Abhishek <abhishekmittapelly0253@gmail.com>
2025-11-05 09:14:29 +0530

    key added

--- users_pro/users_pro/settings.py
+++ users_pro/users_pro/settings.py
@@ -145,5 +145,5 @@ DEFAULT_FILE_STORAGE='cloudinary_storage.storage.medialCloudinaryStorage'
 CLOUDINARY_STORAGE={
     'CLOUD_NAME':env('CLOUDINARY_CLOUD_NAME'),
     'API_KEY':env('CLOUDINARY_API_KEY'),
-    'API_SECRET':env('CLOUDINARY_API_SECRECT')
+    'API_SECRET':env('CLOUDINARY_API_SECRET')
 }
